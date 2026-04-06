9464bf2627b78de12a1e13782ed40f1b7637d5ea
Connor Reger <csr51@pitt.edu>
2026-04-06 13:47:01 -0400

    Change print statement

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,17 +1,21 @@
 public class CommitCafe {
 
-    private static String barista = "Nadine"; // STUDENT_TODO_1A: Change name
+    private static String barista = "Connor"; // STUDENT_TODO_1A: Change name
     // STUDENT_TODO_1B: Add a nickname or title for the barista
+    private static String title = "barista";
 
     private static int cups = 0;
 
     public static void brew(String drink) {
         // STUDENT_TODO_2A: Implement - add 1 to cups & print a message
+        cups++;
+        System.out.println("Order ready!");
         // STUDENT_TODO_2B: Add a second brew-related improvement
+        System.out.println("The " + title + " brewed a nice " + drink);
     }
 
     public static void printSummary() {
-        System.out.println("[SUMMARY] " + barista + " brewed " + cups + " cups today.");
+        System.out.println("Summary: " + cups + " cups were brewed by " + barista + " the " + title + " today.");
     }
 
     public static void main(String[] args) {
